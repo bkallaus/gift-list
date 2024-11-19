833ccc9b29eb8e2675bee7b82dd156916d795e1e
bkallaus <benkallaus@gmail.com>
2024-11-18 19:48:31 -0600

    feat: generate slug

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,7 +1,8 @@
 "use server";
 import { Claims, getSession } from "@auth0/nextjs-auth0";
-import { notFound, redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 import { executeQuery } from "./database";
+import { v4 } from "uuid";
 
 const checkIfUserExistsOrInsert = async (user: Claims) => {
 	const query = "select * from users where email = $1;";
@@ -10,9 +11,15 @@ const checkIfUserExistsOrInsert = async (user: Claims) => {
 	const result = await executeQuery(query, variables);
 
 	if (!result.length) {
+		const slug = v4();
 		const insertQuery =
-			"insert into users (email, first_name, last_name) values ($1, $2, $3);";
-		const insertVariables = [user.email, user.given_name, user.family_name];
+			"insert into users (email, slug, first_name, last_name) values ($1, $2, $3);";
+		const insertVariables = [
+			user.email,
+			slug,
+			user.given_name,
+			user.family_name,
+		];
 
 		await executeQuery(insertQuery, insertVariables);
 	}
